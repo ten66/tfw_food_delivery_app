1dfe0f0424201f46cf535a6dcf4ac6bbe72ff3fd
ten66 <ikesho.1217@gmail.com>
2023-08-14 22:18:33 +0900

    add: :art: #2 CategoryTitle Widgetのインスタンス変数の追加

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class HomeScreen extends StatelessWidget {
           ),
           Row(
             children: [
-              CategoryTitle(),
+              CategoryTitle(title: 'All', active: true),
             ],
           ),
         ],
@@ -68,8 +68,13 @@ class HomeScreen extends StatelessWidget {
 }
 
 class CategoryTitle extends StatelessWidget {
+  final String title;
+  final bool active;
   const CategoryTitle({
     super.key,
+    required this.title,
+    // default value が入る時は、requiredは必要ない
+    this.active = false,
   });
 
   @override
@@ -77,8 +82,10 @@ class CategoryTitle extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 20),
       child: Text(
-        'All',
-        style: Theme.of(context).textTheme.labelLarge,
+        title,
+        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+            // 三項演算子(boolean値 ? trueの場合の処理 : falseの場合の処理)
+            color: active ? kPrimaryColor : kTextColor.withOpacity(.4)),
       ),
     );
   }
