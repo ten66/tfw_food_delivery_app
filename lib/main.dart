e03e46261412fa81b9be4b23bbe98c753b045602
ten66 <ikesho.1217@gmail.com>
2023-08-17 23:17:46 +0900

    add: :art: #2 plussButtonを追加 ~11:20

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Container(
+        padding: const EdgeInsets.all(10),
+        height: 80,
+        width: 80,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: kPrimaryColor.withOpacity(.26),
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: kPrimaryColor,
+          ),
+          child: SvgPicture.asset('assets/icons/plus.svg'),
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
