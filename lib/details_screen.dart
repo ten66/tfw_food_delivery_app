7034e70101a65f18484f2523b6e5912477be9b54
ten66 <ikesho.1217@gmail.com>
2023-08-21 22:50:19 +0900

    add: :art: #3 menuアイコンとarrowアイコンを追加 ~13:42

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/backward.svg',
+                  height: 11,
+                ),
+                SvgPicture.asset(
+                  'assets/icons/menu.svg',
+                  height: 11,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
