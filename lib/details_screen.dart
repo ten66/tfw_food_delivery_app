7e39ef4dd51241b3fa82aa7d4c3bd27da09f8cab
ten66 <ikesho.1217@gmail.com>
2023-08-23 14:53:34 +0900

    add: :art: #3 商品詳細textの追加 ~15:43

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -71,6 +71,10 @@ class DetailsScreen extends StatelessWidget {
                 ),
               ],
             ),
+            const SizedBox(height: 20),
+            const Text(
+              "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+            )
           ],
         ),
       ),
