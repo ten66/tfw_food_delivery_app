9f03ec74206f67643abacd796bac2966824a7dfd
ten66 <ikesho.1217@gmail.com>
2023-08-23 22:20:19 +0900

    add: :art: #3 style調整 ~18:50

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -26,7 +26,7 @@ class DetailsScreen extends StatelessWidget {
               ],
             ),
             Container(
-              margin: const EdgeInsets.symmetric(vertical: 20),
+              margin: const EdgeInsets.symmetric(vertical: 30),
               padding: const EdgeInsets.all(6),
               height: 305,
               width: 305,
@@ -75,74 +75,78 @@ class DetailsScreen extends StatelessWidget {
             const Text(
               "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 27),
-                  decoration: BoxDecoration(
-                    color: kPrimaryColor.withOpacity(.19),
-                    borderRadius: BorderRadius.circular(27),
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Add to bag',
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                      const SizedBox(width: 30),
-                      SvgPicture.asset(
-                        'assets/icons/forward.svg',
-                        height: 11,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 80,
-                  width: 80,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: kPrimaryColor.withOpacity(.26),
-                  ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(15),
-                        height: 60,
-                        width: 60,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: kPrimaryColor,
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 27),
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor.withOpacity(.19),
+                      borderRadius: BorderRadius.circular(27),
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Add to bag',
+                          style: Theme.of(context).textTheme.labelLarge,
                         ),
-                        child: SvgPicture.asset('assets/icons/bag.svg'),
-                      ),
-                      Positioned(
-                        right: 15,
-                        bottom: 10,
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 28,
-                          width: 28,
+                        const SizedBox(width: 30),
+                        SvgPicture.asset(
+                          'assets/icons/forward.svg',
+                          height: 11,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: kPrimaryColor.withOpacity(.26),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(15),
+                          height: 60,
+                          width: 60,
                           decoration: const BoxDecoration(
                             shape: BoxShape.circle,
-                            color: kWhiteColor,
+                            color: kPrimaryColor,
                           ),
-                          child: Text(
-                            '0',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge!
-                                .copyWith(color: kPrimaryColor, fontSize: 16),
+                          child: SvgPicture.asset('assets/icons/bag.svg'),
+                        ),
+                        Positioned(
+                          right: 15,
+                          bottom: 10,
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 28,
+                            width: 28,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: kWhiteColor,
+                            ),
+                            child: Text(
+                              '0',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge!
+                                  .copyWith(color: kPrimaryColor, fontSize: 16),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
